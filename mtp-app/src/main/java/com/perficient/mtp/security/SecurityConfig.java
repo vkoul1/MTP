270e520f25114b37f41c5503db0c63a017fa19d4
PERFICIENT\brett.phillips <brett.phillips@perficient.com>
2021-09-28 21:52:06 -0400

    Better Spring Security OAuth2 config, and cleaned up UI

--- mtp-app/src/main/java/com/perficient/mtp/security/SecurityConfig.java
+++ mtp-app/src/main/java/com/perficient/mtp/security/SecurityConfig.java
@@ -1,9 +1,15 @@
 package com.perficient.mtp.security;
 
+import java.net.URI;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler;
+import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
+import org.springframework.security.web.server.authentication.logout.RedirectServerLogoutSuccessHandler;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 
 @EnableWebFluxSecurity
 public class SecurityConfig {
@@ -11,14 +17,28 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain configure(ServerHttpSecurity http) throws Exception {
     	
-    	
-        http.authorizeExchange()
-                .pathMatchers("/", "/error", "/logout", "/webjars/**").permitAll()
+        return http.authorizeExchange()
+                .pathMatchers("/", "/error", "/images/*", "/webjars/**").permitAll()
                 .anyExchange().authenticated()
                 .and().oauth2Login()
-                .and().csrf().disable()
-                ;
-        
-        return http.build();
+                		.authenticationSuccessHandler(mtpAuthenticationSuccessHandler("/mtp/home.html"))
+                .and().logout()
+                		.logoutSuccessHandler(mtpServerLogoutSuccessHandler("/"))
+                .and().csrf()
+                		.disable()
+                .build();
+    }
+    
+    
+    public ServerAuthenticationSuccessHandler mtpAuthenticationSuccessHandler(String uri) {
+    	return new RedirectServerAuthenticationSuccessHandler(uri);
+    }
+    
+    
+    public ServerLogoutSuccessHandler mtpServerLogoutSuccessHandler(String uri) {
+    	
+    	RedirectServerLogoutSuccessHandler h = new RedirectServerLogoutSuccessHandler();
+    	h.setLogoutSuccessUrl(URI.create(uri));
+    	return h;
     }
 }
